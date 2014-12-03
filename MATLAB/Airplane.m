194d1e89de13aea42314a11c29df5e086cdd4197
Bob Vergauwen <bob.vergauwen@student.kuleuven.be>
2014-12-03 20:07:11 +0100

    a

--- MATLAB/Airplane.m
+++ MATLAB/Airplane.m
@@ -8,24 +8,41 @@ classdef Airplane
         windY = [];
         X = [];
         Y = [];
+        sun = [];
         power = 0;
         xStart = 0;
         yStart = 0;
         xDestin = 0;
         yDestin = 0;
+        xEnd = 1;
+        yEnd = 1;
+        electricEnergy = 1;
     end
     methods
-        function obj = Airplane(clouds,windX,windY,X,Y)
+        function obj = Airplane(clouds,windX,windY,X,Y,sun)
             obj.clouds = clouds;
             obj.windX = windX;
             obj.windY = windY;
             obj.X = X;
             obj.Y = Y;
+            obj.sun = sun;
         end % Constructor method
         function obj = SetStartPosition(x,y)
             obj.xStart = x;
             obj.YStart = y;
         end
+        function obj = SetEndPosition(x,y)
+            obj.xEnd = x;
+            obj.YEnd = y;
+        end
+        function obj = cost(path)
+            % A function to calculate the cast assosiated to a path. The
+            % path is defined as a 3 by m vector with the x, y, t
+            % coorditante in it.
+            
+            
+        end
+        
     end
     
     
--- MATLAB/Static_Weather.m
+++ MATLAB/Static_Weather.m
@@ -1,4 +1,4 @@
-function [ Vcloud,VwindX,VwindY,X,Y ] = Static_Weather( seed,n,dx )
+function [ Vcloud,VwindX,VwindY,X,Y ] = Static_Weather( seed,n ,m)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 rng(seed)
@@ -7,44 +7,69 @@ rng(seed)
 
 [Xdummy,Ydummy] = meshgrid(0:n);
 V = rand(n+1);
-
-[X,Y] = meshgrid(0:dx:n);
+mesh(V)
+[X,Y] = meshgrid(linspace(0,n,m));
 Vcloud = interp2(Xdummy,Ydummy,V,X,Y);
+surf(Vcloud)
 [N,~] = size(Vcloud);
 
-X = X./n;
-Y = Y./n;
+
 m = max([max(max(Vcloud)),abs(min(min(Vcloud)))]);
 Vcloud = Vcloud./m;
 
+% Generating the wind
 
-
-%% Generating the wind
-V = rand(n+1);
 [Xdummy,Ydummy] = meshgrid(0:n);
-[X,Y] = meshgrid(0:dx:n);
-VwindX = interp2(Xdummy,Ydummy,V,X,Y);
 V = rand(n+1);
+mesh(V)
+[X,Y] = meshgrid(linspace(0,n,m));
+VwindX = interp2(Xdummy,Ydummy,V,X,Y);
+surf(VwindX)
+[N,~] = size(VwindX);
+
+
+m = max([max(max(VwindX)),abs(min(min(VwindX)))]);
+VwindX = VwindX./m;
+[Xdummy,Ydummy] = meshgrid(0:n);
+V = rand(n+1);
+mesh(V)
+[X,Y] = meshgrid(linspace(0,n,m));
 VwindY = interp2(Xdummy,Ydummy,V,X,Y);
+surf(VwindY)
+[N,~] = size(VwindY);
 
-X = X./n;
-Y = Y./n;
-mx = max([max(max(VwindX)),abs(min(min(VwindX)))]);
-my = max([max(max(VwindY)),abs(min(min(VwindY)))]);
-VwindX = VwindX./mx;
-VwindY = VwindY./my;
-figure(2)
+m = max([max(max(VwindY)),abs(min(min(VwindY)))]);
+VwindY = VwindY./m;
 
-subplot(1,2,1)
-surf(X,Y,Vcloud);
-title('Cloud density');
-view(0,90)
-hold on
-subplot(1,2,2)
-quiver(X,Y,VwindX,VwindY);
-axis([0,1,0,1])
-hold off
-title('Wind direction');
+
+
+
+
+% %% Generating the wind
+% V = rand(n+1);
+% [Xdummy,Ydummy] = meshgrid(0:n);
+% [X,Y] = meshgrid(linspace(0,n,m));
+% VwindX = interp2(Xdummy,Ydummy,V,X,Y);
+% V = rand(n+1);
+% VwindY = interp2(Xdummy,Ydummy,V,X,Y);
+% 
+% X = X./n;
+% Y = Y./n;
+% mx = max([max(max(VwindX)),abs(min(min(VwindX)))]);
+% my = max([max(max(VwindY)),abs(min(min(VwindY)))]);
+% VwindX = VwindX./mx;
+% VwindY = VwindY./my;
+% 
+% % subplot(1,2,1)
+% % surf(X,Y,Vcloud);
+% % title('Cloud density');
+% % view(0,90)
+% % hold on
+% % subplot(1,2,2)
+% % quiver(X,Y,VwindX,VwindY);
+% % axis([0,1,0,1])
+% % hold off
+% % title('Wind direction');
 
 
 
