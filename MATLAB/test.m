d44530ea0a4afd6087bfe90b0017cd130866e847
Bob Vergauwen <bob.vergauwen@student.kuleuven.be>
2014-12-10 22:15:19 +0100

    Dont kwow

--- MATLAB/test.m
+++ MATLAB/test.m
@@ -4,7 +4,7 @@ startLat = 50;
 endLat = 48;
 startLong = 0;
 endLong = 10;
-m = 20;
+m = 30;
 time = 12;
 date = 180;
 
@@ -18,18 +18,23 @@ plane.SetEndPosition(0.9,0.5);
 % Create an initial guess for the path.
 
  
-x = linspace(0.1,0.9,20);
-t = linspace(0,1,20);
-y = -sin(t*2*pi)/4 + 0.5;
+x = linspace(0.1,0.9,m);
+t = linspace(0,1,m);
+y = sin(t*20*pi)/4 + 0.5;
 
-path = [x;y;t]';
+path = [x;y]';
 
 
 plane.posWeight = 100;
 plane.accelerationWeight = 0.001;
 
-qb = zeros(20,4);
-ub = ones(20,4);
+qb = zeros(m,3);
+qb(1,1:2) = [0.1 0.5];
+qb(end,1:2) = [0.9 0.5];
+
+ub = ones(m,3);
+ub(1,1:2) = [0.1 0.5];
+ub(end,1:2) = [0.9 0.5];
 
 [opt ,V] = fmincon(@plane.weatherSpeedCost, path, [],[],[],[],qb,ub);
 
